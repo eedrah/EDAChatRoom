d59c36bfae345010a15cb46dd8947ce3df496930
Ryan Gibbs <ryangibbsjobs@hotmail.com>
2015-05-09 17:34:42 +1200

    enter bound to send button click and image added

--- EDAChatRoom/Scripts/App/signalRConnection.js
+++ EDAChatRoom/Scripts/App/signalRConnection.js
@@ -10,7 +10,14 @@ function runChat() {
     chatroom.client.broadcastMessage = function (username, message, currentTime) {
         var $li = $('<li>').text(createLiText(username, message, currentTime));
         $('#messagesReceived').append($li);
+        $('#messageAlertSound').get(0).play();
     }
+
+    $("#messageBox").keypress(function(e) {
+        if (e.which === 13) {
+            $('#sendMessageButton').trigger('click');
+        }
+    });
 
     $('#sendMessageButton').click(function() {
         sendMessage(chatroom);
