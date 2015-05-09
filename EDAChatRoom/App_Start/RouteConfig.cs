627895f384c1fb23e7da946363252d5d5324788a
Eedrah <eedrah@users.noreply.github.com>
2015-05-09 13:50:35 +1200

    change default route to chat

--- EDAChatRoom/App_Start/RouteConfig.cs
+++ EDAChatRoom/App_Start/RouteConfig.cs
@@ -13,7 +13,7 @@ namespace EDAChatRoom {
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Chat", id = UrlParameter.Optional }
             );
         }
     }
