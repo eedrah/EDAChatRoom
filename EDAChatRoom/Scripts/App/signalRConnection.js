fc4b3a62d43d08acfc7825fa2364422681856408
Ryan Gibbs <ryangibbsjobs@hotmail.com>
2015-05-09 19:02:07 +1200

    Added desktop notification popup

--- EDAChatRoom/Scripts/App/signalRConnection.js
+++ EDAChatRoom/Scripts/App/signalRConnection.js
@@ -4,13 +4,16 @@ $(runChat);
 
 function runChat() {
     username = prompt('Enter a groovy alias');
-
+    Notification.requestPermission();
     var chatroom = $.connection.chatroom;
 
     chatroom.client.broadcastMessage = function (username, message, currentTime) {
         var $li = $('<li>').text(createLiText(username, message, currentTime));
         $('#messagesReceived').append($li);
         $('#messageAlertSound').get(0).play();
+        //need to add condition to check if user is currently on the window or not
+        notifyMe(username, message);
+  
     }
 
     $("#messageBox").keypress(function(e) {
@@ -36,4 +39,16 @@ function sendMessage(chatroom) {
         chatroom.server.send(username, $messageBox.val());
     };
     $messageBox.val('');
+}
+
+function notifyMe(username, message) {
+
+    var popup = new Notification('You have a message from ' + username, {
+       icon: 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAJiAAAAJGQ5MGM3MzIwLTA5NzUtNDE4Yy04ZTkzLTY3YjgyY2Y1ODcxZA.jpg',
+       body: message
+    });
+    setTimeout(function() {
+        popup.close();
+    }, 5000);
+
 }
