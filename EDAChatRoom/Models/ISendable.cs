b2de6c53028392f2c55611fe167a59ba68fd2f34
Eedrah <eedrah@users.noreply.github.com>
2015-05-09 19:02:55 +1200

    create interface for sending messages

---
+++ EDAChatRoom/Models/ISendable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDAChatRoom.Models {
+    public interface ISendable {
+        InfoType InfoType { get; set; }
+    }
+
+    public enum InfoType
+    {
+        Message,
+        Disconnection
+    }
+}
