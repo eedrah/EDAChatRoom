b920ba6acc02d259b671b163d1e7c6a178dc2852
Eedrah <eedrah@users.noreply.github.com>
2015-05-09 13:36:44 +1200

    configure basic controller

--- EDAChatRoom/Controllers/HomeController.cs
+++ EDAChatRoom/Controllers/HomeController.cs
@@ -6,19 +6,7 @@ using System.Web.Mvc;
 
 namespace EDAChatRoom.Controllers {
     public class HomeController : Controller {
-        public ActionResult Index() {
-            return View();
-        }
-
-        public ActionResult About() {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact() {
-            ViewBag.Message = "Your contact page.";
-
+        public ActionResult Chat() {
             return View();
         }
     }
