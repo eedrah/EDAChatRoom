37686c6d68cb892a2a1b51a9fec3d4c41a5c9195
Eedrah <eedrah@users.noreply.github.com>
2015-05-09 14:54:43 +1200

    write javascript

--- EDAChatRoom/Hubs/EDAChatHub.cs
+++ EDAChatRoom/Hubs/EDAChatHub.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
 
 namespace EDAChatRoom.Hubs {
+    [HubName("chatroom")]
     public class EDAChatHub : Hub {
         public void Send(string name, string message) {
             Clients.All.broadcastMessage(name, message);
--- EDAChatRoom/Scripts/App/signalRConnection.js
+++ EDAChatRoom/Scripts/App/signalRConnection.js
@@ -1,1 +1,32 @@
-﻿console.log('hhi');
+﻿var username;
+
+$(runChat);
+
+function runChat() {
+    username = prompt('Enter a groovy alias')
+
+    var chatroom = $.connection.chatroom;
+
+    chatroom.client.broadcastMessage = function (username, message) {
+        var $li = $('<li>').text(createLiText(username, message));
+        $('#messagesReceived').append($li);
+    }
+
+    $('#sendMessageButton').click(function() {
+        sendMessage(chatroom);
+    });
+
+    $.connection.hub.start();
+};
+
+function createLiText(username, message) {
+    return "From " + username + ": " + message;
+}
+
+function sendMessage(chatroom) {
+    var $messageBox = $('#messageBox');
+    if ($messageBox.val() != '') {
+        chatroom.server.send(username, $messageBox.val());
+    };
+    $messageBox.val('');
+}
