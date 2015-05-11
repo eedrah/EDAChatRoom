b262db62a55fdbee8602d58e09b7380727dd6aec
Ryan Gibbs <ryangibbsjobs@hotmail.com>
2015-05-12 09:40:13 +1200

    removed pointless comment

--- EDAChatRoom/Scripts/App/signalRConnection.js
+++ EDAChatRoom/Scripts/App/signalRConnection.js
@@ -9,15 +9,13 @@ function runChat() {
     Notification.requestPermission();
 
     chatroom.client.serverSend = function (hubMessage) {
+        var payload = hubMessage.Payload;
         if (hubMessage.HubMessageType === "Message") {
-            controller.RenderMessage(hubMessage.Payload.Username, hubMessage.Payload.MessageText, hubMessage.MessageTime);
+            controller.RenderMessage(payload.Username, payload.MessageText, hubMessage.MessageTime);
         } else {
             console.log(hubMessage);
         }
-    }
-
-        ////need to add condition to check if user is currently on the window or not
-        
+    }      
 
     $("#messageBox").keypress(function(e) {
         if (e.which === 13) {
