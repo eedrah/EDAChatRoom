2ef877e0166b959ca44f115567a3eed82758c7de
Ryan Gibbs <ryangibbsjobs@hotmail.com>
2015-05-12 14:20:44 +1200

    Now replaces oldest message with the newest, BOOMBADABOOMBABY!

--- EDAChatRoom/Controllers/DbRecentMessagesController.cs
+++ EDAChatRoom/Controllers/DbRecentMessagesController.cs
@@ -21,7 +21,7 @@ namespace EDAChatRoom.Controllers
             dbcontext.SaveChanges();
             while(recentMessagesTable.Count() >= 30)
             {
-                DbRecentMessage messageToBeDeleted = recentMessagesTable.OrderByDescending(r => r.Id).First();
+                DbRecentMessage messageToBeDeleted = recentMessagesTable.OrderBy(r => r.MessageTime).First();
                 dbcontext.RecentMessages.Remove(messageToBeDeleted);
                 dbcontext.SaveChanges();
             }
