c384452a2688a2f88922ebf7c2e010747f064f01
Ryan Gibbs <ryangibbsjobs@hotmail.com>
2015-05-13 12:41:25 +1200

    fixed optimistic concurrency error, added try and catch exception, working

--- EDAChatRoom/Controllers/DbRecentMessagesController.cs
+++ EDAChatRoom/Controllers/DbRecentMessagesController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.Core;
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -26,9 +29,17 @@ namespace EDAChatRoom.Controllers
             DbSet<DbRecentMessage> recentMessagesTable = dbcontext.RecentMessages;
             while (recentMessagesTable.Count() > 30)
             {
-                DbRecentMessage messageToBeDeleted = recentMessagesTable.OrderBy(r => r.MessageTime).First();
-                dbcontext.RecentMessages.Remove(messageToBeDeleted);
-                dbcontext.SaveChanges();
+                try
+                {
+                    DbRecentMessage messageToBeDeleted = recentMessagesTable.OrderBy(r => r.MessageTime).First();
+                    dbcontext.RecentMessages.Remove(messageToBeDeleted);
+                    dbcontext.SaveChanges();
+                }
+                catch(OptimisticConcurrencyException)
+                {
+                    var ctx = ((IObjectContextAdapter) recentMessagesTable).ObjectContext;
+                    ctx.Refresh(RefreshMode.ClientWins, recentMessagesTable);
+                }
             }
         }
     }
