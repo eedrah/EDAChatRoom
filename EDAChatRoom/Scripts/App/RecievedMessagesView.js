6a3c07784a3811a23418782082dff5b6d88eadd4
Ryan Gibbs <ryangibbsjobs@hotmail.com>
2015-05-19 11:10:19 +1200

    recieved images refactred and now working

--- EDAChatRoom/Scripts/App/RecievedMessagesView.js
+++ EDAChatRoom/Scripts/App/RecievedMessagesView.js
@@ -3,23 +3,33 @@
 }
 
 RMView.prototype.RenderMessageToPage = function ($liElement) {
-    $('#messagesReceived').append($liElement);
+    $("#messagesReceived").append($liElement);
     ScrollToBottomOfReceivedMessages();
 }
 
 RMView.prototype.CreateMessageAnnouncingNewConnectedUser = function (connectedUser) {
     var $li = $("<li>").text(connectedUser.TimeConnected + " , " + connectedUser.UserName + " is now Connected");
-    $('#messagesReceived').append($li);
+    $("#messagesReceived").append($li);
 }
 
 RMView.prototype.ShowOnlineUsers = function (listOfUsers) {
     this.ResetOnlineUsersList();
     for (var i = 0; i < listOfUsers.length; i++) {
-        $li = $('<li>').text(listOfUsers[i]);
-        $('#listOfConnectedUsers').append($li);
+        $li = $("<li>").text(listOfUsers[i]);
+        $("#listOfConnectedUsers").append($li);
     }
 }
 
 RMView.prototype.ResetOnlineUsersList = function () {
-    $('#listOfConnectedUsers').empty();
+    $("#listOfConnectedUsers").empty();
+}
+
+RMView.prototype.ShowImageInChatBox = function (imageMessage) {
+    var image = new Image();
+    var messages = $("#messagesReceived");
+    var $li = $("<li>").text("image recieved from " + imageMessage.Username);
+
+    image.src = imageMessage.ImageBase64;
+    $li.append(image);
+    messages.append($li);
 }
